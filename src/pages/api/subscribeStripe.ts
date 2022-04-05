34d9694f5f5a01c84fa08dfe8a330c20f06c3ef5
Marcella <marcella.amorimsa@gmail.com>
2022-04-05 07:04:16 -0300

    fix: impedir duplicacao de clientes no stripe

--- src/pages/api/subscribeStripe.ts
+++ src/pages/api/subscribeStripe.ts
@@ -1,19 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from 'next-auth/client';
+import { fauna } from "../../services/fauna";
+import { query } from 'faunadb';
 
 import { stripe } from "../../services/stripe";
+
+type IUser = {
+    ref: { id: string; };
+    ts: any;
+    data: {
+        email: string;
+        stripe_customer_id?: string;
+    };
+}
 
 const subscribeStripe = async (request: NextApiRequest, response: NextApiResponse) => {
     if (request.method === "POST") {
         const session = await getSession({ req: request });
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            /* metadata:  */
-        })
+        const user = await fauna.query<IUser>(
+            query.Get(
+                query.Match(
+                    query.Index('find_user_by_email'),
+                    query.Casefold(session.user.email)
+                )
+            )
+        );
+
+        let customerIdFromStripe = user.data.stripe_customer_id;
+
+        if (!customerIdFromStripe) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                /* metadata:  */
+            });
+
+            await fauna.query(
+                query.Update(
+                    query.Ref(query.Collection('users'), user.ref.id),
+                    {
+                        data: { stripe_customer_id: stripeCustomer.id }
+                    }
+                )
+            );
+
+            customerIdFromStripe = stripeCustomer.id;
+        }
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerIdFromStripe,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{
